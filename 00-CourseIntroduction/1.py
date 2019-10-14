bd8b8b8caf02158f7ab9c4af92b9c95fb7db976e
Marcin Bruzda <marcin.bru@op.pl>
2019-10-14 20:53:48 +0200

    1

--- 00-CourseIntroduction/1.py
+++ 00-CourseIntroduction/1.py
@@ -1,4 +1,5 @@
-# hehe
-
- 
-print ("Lubie placki")
+from math import *
+promien = 5
+obwod = 2*pi*promien
+pole = promien**2*pi
+print(f"\n Pole kola o promieniu {promien} wynosi {pole} \n Obwod kola o promienu {promien} wynosi {obwod}" )
---
+++ 01-TypesAndVariables/temperatura.py
@@ -0,0 +1,7 @@
+cel = input("Podaj temperature w skali celcjusza: ")
+
+cel = float(cel)
+f = cel * 1.8 + 32
+k = cel + 293
+
+print(f"Temperatura w skali Faranheita to {f} \n Temperatura w skali Kelvina to {k}")
