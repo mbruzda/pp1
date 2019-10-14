49bedbea82f7cd932f6a4597293e69db8f031834
Marcin Bruzda <marcin.bru@op.pl>
2019-10-14 19:50:03 +0200

    Promien koła

---
+++ 01-TypesAndVariables/parametry_kola.py
@@ -0,0 +1,6 @@
+from math import *
+promien = input("Wprowadz promien")
+promien = int(promien)
+obwod = 2*pi*promien
+pole = promien**2*pi
+print(f"\n Pole kola o promieniu {promien} wynosi {pole} \n Obwod kola o promienu {promien} wynosi {obwod}" )
